b85e875eda66dc4568bcd82d7f612e7057c6b2db
TuanTran <tqt2285@gmail.com>
2022-05-29 09:09:34 +0700

    init service

---
+++ src/Services/Catalog/Catalog.API/Data/Interfaces/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data.Interfaces
+{
+    public interface ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+    }
+}
