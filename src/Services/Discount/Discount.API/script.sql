c8f65773765238728d553dc8ca20ac95477a5696
TuanTran <tqt2285@gmail.com>
2022-05-29 23:22:38 +0700

    discount api

---
+++ src/Services/Discount/Discount.API/script.sql
@@ -0,0 +1,16 @@
+﻿--start pgamdin /  Postgresql -  http://localhost:5050/
+DiscountServer / DiscountDb / admin / admin1234
+
+/*CREATE TABLE Coupon(
+		ID SERIAL PRIMARY KEY         NOT NULL,
+		ProductName     VARCHAR(24) NOT NULL,
+		Description     TEXT,
+		Amount          INT
+	);
+INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('IPhone X', 'IPhone Discount', 150);
+
+INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('Samsung 10', 'Samsung Discount', 100);
+*/
+
+SELECT * FROM public.coupon
+	ORDER BY id ASC 
